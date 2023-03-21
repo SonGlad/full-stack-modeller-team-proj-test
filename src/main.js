e8c784c7e21dbedd2c31fbb9c2f70e0fa5fdbb1d
SonGlad <okoshevy@gmail.com>
2023-03-21 17:19:01 +0200

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-import "./js/slick"
-import "./js/mobile"
-import "./js/form-validation"
-import "./js/header-&-scroll-to-top"
-import "./js/modals"
-import "./js/store.js"
+// import "./js/slick"
+// import "./js/mobile"
+// import "./js/form-validation"
+// import "./js/header-&-scroll-to-top"
+// import "./js/modals"
+// import "./js/store.js"
 
 
