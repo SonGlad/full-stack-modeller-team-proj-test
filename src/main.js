0fc7228ede8439dbdcd2a8b5ef8be0e5cb9b4167
hopeofukraine <121205584+hopeofukraine@users.noreply.github.com>
2023-03-24 15:25:39 +0100

    section-contact

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
  import "./js/header-after-btn"
  import "./js/slick"
+ import "./js/form-validation"
 // import "./js/mobile"
-// import "./js/form-validation"
 // import "./js/header-&-scroll-to-top"
 // import "./js/modals"
 // import "./js/store.js"
