1b70cc76661bab3fdca1100d3d57e4704a0bbce9
Eugeshakw <eugeshakw@gmail.com>
2023-03-25 02:52:28 +0200

    update-slick

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,21 +1,12 @@
-$('.company-swipe').slick({
-
-  prevArrow: '<button type="button" class="my-slick-prev">&xlarr;</button>',
-  nextArrow: '<button type="button" class="my-slick-next">&xrarr;</button>',
-
-  slidesToShow: 4,
+var $jq = jQuery.noConflict();
+$(document).ready(function(){
+  $(".list-card-team").slick({
+  // dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 6,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000,
-
-  responsive: [
-    {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 2,
-      }
-    },
-  ]
-
-
+  autoplaySpeed: 1000
+})
 });
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+    
+ 
+ import "./js/slick-our-team"
  import "./js/header-after-btn"
  import "./js/slick"
  import "./js/form-validation"
+ 
 // import "./js/mobile"
 // import "./js/header-&-scroll-to-top"
 // import "./js/modals"
