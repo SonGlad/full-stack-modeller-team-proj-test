5174f8e6b52f9bc481ed2173b63c851eb527ee5a
Eugeshakw <eugeshakw@gmail.com>
2023-03-25 19:37:15 +0200

    update-slick

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,12 +1,22 @@
 var $jq = jQuery.noConflict();
-$(document).ready(function(){
-  $(".list-card-team").slick({
-  // dots: true,
+// $(document).ready(function(){
+//   $(".list-card-team").slick({
+//   // dots: true,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 6,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 1000
+// })
+// });
+
+$('.list-card-team').slick({
   infinite: true,
-  speed: 500,
-  slidesToShow: 6,
+  slidesToShow: 6.3,
   slidesToScroll: 1,
+  speed: 500, 
   autoplay: true,
-  autoplaySpeed: 1000
-})
+  autoplaySpeed: 1000, 
 });
+
