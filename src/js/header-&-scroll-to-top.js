a29b29946a1d35a60e3fcb8d0dd00449830c46c0
SonGlad <okoshevy@gmail.com>
2023-06-29 05:01:54 +0300

    revert

--- src/js/header-&-scroll-to-top.js
+++ src/js/header-&-scroll-to-top.js
@@ -1,24 +1,24 @@
-let btn_scroll = document.querySelector('.btn_scroll')
+// let btn_scroll = document.querySelector('.btn_scroll')
     
-     // Button Scroll
+//      // Button Scroll
 
-  window.onscroll = function () {
+//   window.onscroll = function () {
     
-    if (window.screen.width >= 1280) {
-      scrollY <= 815
-      ? document.querySelector('.header-js').classList.remove('header-bg')
-      : document.querySelector('.header-js').classList.add('header-bg');
+//     if (window.screen.width >= 1280) {
+//       scrollY <= 815
+//       ? document.querySelector('.header-js').classList.remove('header-bg')
+//       : document.querySelector('.header-js').classList.add('header-bg');
       
-    } else {
-      scrollY <= 120
-      ? document.querySelector('.header-js').classList.remove('header-bg')
-      : document.querySelector('.header-js').classList.add('header-bg');
+//     } else {
+//       scrollY <= 120
+//       ? document.querySelector('.header-js').classList.remove('header-bg')
+//       : document.querySelector('.header-js').classList.add('header-bg');
       
-    }
-      // Scroll to Top                                                      
+//     }
+//       // Scroll to Top                                                      
       
-    if (window.pageYOffset > 200) {
-    btn_scroll.style.opacity = '1'
-    } else { btn_scroll.style.opacity = '0' }
+//     if (window.pageYOffset > 200) {
+//     btn_scroll.style.opacity = '1'
+//     } else { btn_scroll.style.opacity = '0' }
     
-  };
+//   };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
 
- import "./js/header-after-btn"
- import "./js/slick"
- import "./js/header-join-us-btn"
- import "./js/header-nav-btns"
-
-
-
-import "./js/mobile"
-// import "./js/header-&-scroll-to-top"
-// import "./js/modals"
-// import "./js/store.js"
+import "./js/mobMenu";
     
 
